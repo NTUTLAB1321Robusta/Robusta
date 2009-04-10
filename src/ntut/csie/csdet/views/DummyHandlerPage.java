aa1eea3d948658a5621824e8aca622173d5ed4cf
shiau <shiau@98712cce-040b-4e5c-8357-9a3231c88d43>
2009-04-10 05:12:37 +0000

    Dummy handler Setting page可以有兩種情形的範例 (Label名稱還沒取好)
    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT@773 98712cce-040b-4e5c-8357-9a3231c88d43

--- src/ntut/csie/csdet/views/DummyHandlerPage.java
+++ src/ntut/csie/csdet/views/DummyHandlerPage.java
@@ -7,6 +7,8 @@ import ntut.csie.csdet.preference.JDomUtil;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -22,37 +24,54 @@ import org.jdom.Element;
 public class DummyHandlerPage extends APropertyPage{
 	// 放code template的區域
 	private StyledText templateArea;
-	// code template的內容
-	private String templateText;
+	// 放code template的區域
+	private StyledText templateArea2;
 	// 是否要捕捉System.out.println() and print()的按鈕
 	private Button systemoutprintlnButton;
 	// 是否要捕捉log4j的button
 	private Button log4jButton;
 	// 是否要捕捉java.util.logging的button
 	private Button javaUtillogBtn;
+	//
+	StyleRange[] sampleStyles1 = new StyleRange[5];
+	StyleRange[] sampleStyles2 = new StyleRange[7];
+	// code template前半部內容
+	private String mainText;
+	//　code template的結尾"}"
+	private String endText;
+	// code template
+	private String printText;
+	// log4j的button的字串
+	private String logText;
+	// java.util.logging的字串
+	private String javaText;
 	
 	public DummyHandlerPage(Composite composite,CSPropertyPage page){
 		super(composite,page);
 		//程式碼的內容
-		templateText = "try {   \n" +
-		               "  \n"+
-	 	               "} catch (Exception e) { \n" +
-	                   "    System.out.println(e);\n" +
-	                   "    System.out.print(e);\n" +
-		               "    e.printStackTrace();\n"+
-		               "    // using log4j\n" +
-		               "    logger.info(e.getMessage()"+ ");\n"+ 
-		               "    // using java.util.logging.Logger \n" +
-		               "    java_logger.info(e.getMessage()"+ "); \n"+ 
-		               "}";
+		mainText =		"try {   \n" +
+						"    // code in here\n" +
+						"} catch (Exception e) { \n" +
+						"    e.printStackTrace();\n";
+		endText =		"}";
+		printText =		"    System.out.println(e);\n" +
+						"    System.out.print(e);\n";
+		logText =		"    // using log4j\n" +
+						"    logger.info(e.getMessage()"+ ");\n";
+		javaText =		"    // using java.util.logging.Logger \n" +
+						"    java_logger.info(e.getMessage()"+ "); \n";
 		//加入頁面的內容
 		addFirstSection(composite);
 	}
 	
-	private void addFirstSection(Composite dummyHandlerPage){
-		final Label detectSettingsLabel = new Label(dummyHandlerPage, SWT.NONE);
-		detectSettingsLabel.setText("Detect Settings");
-		detectSettingsLabel.setBounds(10, 10, 90, 12);
+	private void addFirstSection(final Composite dummyHandlerPage){
+		final Label detectSettingsLabel1 = new Label(dummyHandlerPage, SWT.NONE);
+		detectSettingsLabel1.setText("Detect Settings");
+		detectSettingsLabel1.setBounds(10, 10, 77, 12);
+		
+		final Label detectSettingsLabel2 = new Label(dummyHandlerPage, SWT.NONE);
+		detectSettingsLabel2.setBounds(228, 10, 77, 12);
+		detectSettingsLabel2.setText("Detect Settings");
 		
 		Document docJDom = JDomUtil.readXMLFile();
 		String setting = "";
@@ -68,86 +87,178 @@ public class DummyHandlerPage extends APropertyPage{
 		
 		systemoutprintlnButton = new Button(dummyHandlerPage, SWT.CHECK);
 		systemoutprintlnButton.setText("System.out.print();");
-		systemoutprintlnButton.setBounds(20, 28, 123, 16);
+		systemoutprintlnButton.setBounds(20, 28, 202, 16);
+		systemoutprintlnButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				//按下按鈕而改變Text文字和顏色
+				adjustText();
+				adjustFont();
+			}
+		});
 		if(setting.equals("Y")){
 			systemoutprintlnButton.setSelection(true);
 		}
 
 		log4jButton = new Button(dummyHandlerPage, SWT.CHECK);
+		log4jButton.setBounds(238, 28, 202, 16);
 		log4jButton.setText("Detect using org.apache.log4j");
-		log4jButton.setBounds(20, 50, 160, 16);
+		log4jButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				//按下按鈕而改變Text文字和顏色
+				adjustText();
+				adjustFont();
+			}
+		});
 		if(log4jSet.equals("Y")){
 			log4jButton.setSelection(true);
 		}
 
 		javaUtillogBtn = new Button(dummyHandlerPage, SWT.CHECK);
 		javaUtillogBtn.setText("Detect using java.util.logging.Logger");
-		javaUtillogBtn.setBounds(20, 72, 190, 16);
-		if(javaLogSet.equals("Y")){
+		javaUtillogBtn.setBounds(238, 48, 202, 16);
+		javaUtillogBtn.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				//按下按鈕而改變Text文字和顏色
+				adjustText();
+				adjustFont();
+			}
+		});
+		if(javaLogSet.equals("Y")) {
 			javaUtillogBtn.setSelection(true);
 		}
 		
-		final Label codeTemplateLabel = new Label(dummyHandlerPage, SWT.NONE);
-		codeTemplateLabel.setText("Code Template:");
-		codeTemplateLabel.setBounds(10, 100, 96, 12);
+		final Label codeTemplateLabel1 = new Label(dummyHandlerPage, SWT.NONE);
+		codeTemplateLabel1.setText("Code Template:");
+		codeTemplateLabel1.setBounds(10, 82, 96, 12);
+		
+		final Label codeTemplateLabel2 = new Label(dummyHandlerPage, SWT.NONE);
+		codeTemplateLabel2.setBounds(10, 232, 96, 12);
+		codeTemplateLabel2.setText("Code Template:");
 
-		final Label label = new Label(dummyHandlerPage, SWT.SEPARATOR| SWT.HORIZONTAL);
-		label.setBounds(10, 112, 472, 12);
+		final Label label1 = new Label(dummyHandlerPage, SWT.SHADOW_IN | SWT.HORIZONTAL | SWT.SEPARATOR);
+		label1.setBounds(10, 218, 464, 12);
+		final Label label3 = new Label(dummyHandlerPage, SWT.CENTER | SWT.SEPARATOR | SWT.HORIZONTAL);
+		label3.setAlignment(SWT.CENTER);
+		label3.setBounds(0, 68, 484, 12);
 
 		templateArea = new StyledText(dummyHandlerPage, SWT.BORDER);
-		Font font = new Font(dummyHandlerPage.getDisplay(),"Courier New",14,SWT.NORMAL);		
+		Font font = new Font(dummyHandlerPage.getDisplay(),"Courier New",9,SWT.NORMAL);		
 		templateArea.setFont(font);
-		templateArea.setText(templateText);
-		templateArea.setBounds(10, 125, 458, 263);
+		templateArea.setBounds(10, 100, 464, 115);
 		templateArea.setEditable(false);
+		
+		templateArea2 = new StyledText(dummyHandlerPage, SWT.BORDER);
+		templateArea2.setFont(font);
+		templateArea2.setBounds(10, 250, 464, 148);
+		templateArea2.setEditable(false);
+
+		//載入預定的字型、顏色
+		addSampleStyle(dummyHandlerPage.getDisplay());
+
+		//調整Text的文字
+		adjustText();
 
 		//調整程式碼的顏色
-		adjustFont(dummyHandlerPage.getDisplay());
+		adjustFont();
+	}
+	
+	/**
+	 * 調整Text的文字
+	 */
+	private void adjustText()
+	{
+		if(systemoutprintlnButton.getSelection())
+			templateArea.setText(mainText + printText + endText);
+		else
+			templateArea.setText(mainText + endText);
+		if (log4jButton.getSelection() && javaUtillogBtn.getSelection())
+			templateArea2.setText(mainText + logText + javaText + endText);
+		else if (log4jButton.getSelection() && !javaUtillogBtn.getSelection())
+			templateArea2.setText(mainText + logText + endText);
+		else if (!log4jButton.getSelection() && javaUtillogBtn.getSelection())
+			templateArea2.setText(mainText + javaText + endText);
+		else
+			templateArea2.setText(mainText + endText);
+	}
+
+	/**
+	 * 將程式碼中的可能會用到的字型、顏色先行輸入
+	 * @param display
+	 */
+	private void addSampleStyle(Display display) {
+		//Try
+		sampleStyles1[0] = sampleStyles2[0] = new StyleRange();
+		sampleStyles1[0].fontStyle = sampleStyles2[0].fontStyle  = SWT.BOLD;
+		sampleStyles1[0].foreground = sampleStyles2[0].foreground = display.getSystemColor(SWT.COLOR_DARK_MAGENTA);
+		// 註解
+		sampleStyles1[1] = sampleStyles2[1] =  new StyleRange();
+		sampleStyles1[1].fontStyle = sampleStyles2[1].fontStyle = SWT.ITALIC;
+		sampleStyles1[1].foreground = sampleStyles2[1].foreground = display.getSystemColor(SWT.COLOR_DARK_GREEN);
+		//catch
+		sampleStyles1[2] = sampleStyles2[2] = new StyleRange();
+		sampleStyles1[2].fontStyle = sampleStyles2[2].fontStyle = SWT.BOLD;
+		sampleStyles1[2].foreground = sampleStyles2[2].foreground = display.getSystemColor(SWT.COLOR_DARK_MAGENTA);
+		//out
+		sampleStyles1[3] = new StyleRange();
+		sampleStyles1[3].fontStyle = SWT.ITALIC;
+		sampleStyles1[3].foreground = display.getSystemColor(SWT.COLOR_BLUE);
+		// out
+		sampleStyles1[4] = new StyleRange();
+		sampleStyles1[4].fontStyle = SWT.ITALIC;
+		sampleStyles1[4].foreground = display.getSystemColor(SWT.COLOR_BLUE);
+		// 註解
+		sampleStyles2[3] = new StyleRange();
+		sampleStyles2[3].fontStyle = SWT.ITALIC;
+		sampleStyles2[3].foreground = display.getSystemColor(SWT.COLOR_DARK_GREEN);		
+		// log4j
+		sampleStyles2[4] = new StyleRange();
+		sampleStyles2[4].fontStyle = SWT.ITALIC;
+		sampleStyles2[4].foreground = display.getSystemColor(SWT.COLOR_BLUE);
+		// 註解
+		sampleStyles2[5] = new StyleRange();
+		sampleStyles2[5].fontStyle = SWT.ITALIC;
+		sampleStyles2[5].foreground = display.getSystemColor(SWT.COLOR_DARK_GREEN);	
+		// java.util.logging.Logger
+		sampleStyles2[6] = new StyleRange();
+		sampleStyles2[6].fontStyle = SWT.ITALIC;
+		sampleStyles2[6].foreground = display.getSystemColor(SWT.COLOR_BLUE);
 	}
 	
 	/**
 	 * 將程式碼中的Try ,catch,out標上顏色
-	 * @param display
 	 */
-	private void adjustFont(Display display){
-		StyleRange[] styles = new StyleRange[8];
-		//Try
-		styles[0] = new StyleRange();
-		styles[0].fontStyle = SWT.BOLD;
-		styles[0].foreground = display.getSystemColor(SWT.COLOR_DARK_MAGENTA);
-		//catch
-		styles[1] = new StyleRange();
-		styles[1].fontStyle = SWT.BOLD;
-		styles[1].foreground = display.getSystemColor(SWT.COLOR_DARK_MAGENTA);
-		//out
-		styles[2] = new StyleRange();
-		styles[2].fontStyle = SWT.ITALIC;
-		styles[2].foreground = display.getSystemColor(SWT.COLOR_BLUE);
-		// out
-		styles[3] = new StyleRange();
-		styles[3].fontStyle = SWT.ITALIC;
-		styles[3].foreground = display.getSystemColor(SWT.COLOR_BLUE);
-		// 註解
-		styles[4] = new StyleRange();
-		styles[4].fontStyle = SWT.ITALIC;
-		styles[4].foreground = display.getSystemColor(SWT.COLOR_DARK_GREEN);		
-		// log4j
-		styles[5] = new StyleRange();
-		styles[5].fontStyle = SWT.ITALIC;
-		styles[5].foreground = display.getSystemColor(SWT.COLOR_BLUE);
-		// 註解
-		styles[6] = new StyleRange();
-		styles[6].fontStyle = SWT.ITALIC;
-		styles[6].foreground = display.getSystemColor(SWT.COLOR_DARK_GREEN);	
-		// java.util.logging.Logger
-		styles[7] = new StyleRange();
-		styles[7].fontStyle = SWT.ITALIC;
-		styles[7].foreground = display.getSystemColor(SWT.COLOR_BLUE);
+	private void adjustFont(){
+		int[] ranges1;
+		int[] ranges2;
 		
-		int[] ranges = new int[] {0,3,14,5,48,3,75,3,118,14,137,6,170,33,209,11};
-		templateArea.setStyleRanges(ranges, styles);
-	}
+		//SystemOutPrint按鈕有無按下而調整顏色位置(起始位置,幾個字元)兩個一組
+		if (systemoutprintlnButton.getSelection())
+			ranges1 = new int[] {0,3,13,15,31,5,90,3,117,3};
+		else
+			ranges1 = new int[] {0,3,13,15,31,5};
 
+		//因log4j和javaUtilog按鈕有無按下而調整顏色位置
+		if (log4jButton.getSelection() && javaUtillogBtn.getSelection())
+			ranges2 = new int[] {0,3,13,15,31,5,83,14,102,6,135,33,174,11};
+		else if (log4jButton.getSelection() && !javaUtillogBtn.getSelection())
+			ranges2 = new int[] {0,3,13,15,31,5,83,14,102,6};
+		else if (!log4jButton.getSelection() && javaUtillogBtn.getSelection())
+			ranges2 = new int[] {0,3,13,15,31,5,83,33,122,11};
+		else
+			ranges2 = new int[] {0,3,13,15,31,5};
+		
+		//把預設字型風格載入到要使用的字型風格(配合字數而改變)
+		StyleRange[] styles1 = new StyleRange[ranges1.length/2];
+		for (int i=0;i<(ranges1.length/2);i++)
+			styles1[i] = sampleStyles1[i];
+
+		StyleRange[] styles2 = new StyleRange[ranges2.length/2];
+		for (int i=0;i<(ranges2.length/2);i++)
+			styles2[i] = sampleStyles2[i];		
+
+		templateArea.setStyleRanges(ranges1, styles1);
+		templateArea2.setStyleRanges(ranges2, styles2);
+	}
 
 	@Override
 	public boolean storeSettings() {
@@ -163,14 +274,12 @@ public class DummyHandlerPage extends APropertyPage{
 		}else{
 			rule.setAttribute(JDomUtil.systemoutprint,"N");
 		}
-		
 		//假如log4j有被勾選起來
 		if(log4jButton.getSelection()){
 			rule.setAttribute(JDomUtil.apache_log4j,"Y");	
 		}else{
 			rule.setAttribute(JDomUtil.apache_log4j,"N");	
 		}
-		
 		// 假如java.util.logging.Logger有被勾選起來
 		if(javaUtillogBtn.getSelection()){
 			rule.setAttribute(JDomUtil.java_Logger,"Y");	
