98a57fe84a135ca19bd3f6b132f4efe23cabb07e
shiau <shiau@98712cce-040b-4e5c-8357-9a3231c88d43>
2009-05-25 05:54:10 +0000

    新增RL Annotation Order Quick Fix功能，能修正RL annotation父、子類別不正確
    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT@954 98712cce-040b-4e5c-8357-9a3231c88d43

--- src/ntut/csie/rleht/builder/RLQuickFix.java
+++ src/ntut/csie/rleht/builder/RLQuickFix.java
@@ -17,13 +17,13 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IOpenable;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ArrayInitializer;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.IExtendedModifier;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MemberValuePair;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
@@ -89,7 +89,7 @@ public class RLQuickFix implements IMarkerResolution {
 
 				boolean isok = findMethod(marker.getResource(), Integer.parseInt(methodIdx));
 				if (isok) {
-					this.updateRLAnnotation(Integer.parseInt(msgIdx), levelForUpdate);
+					this.updateRLAnnotation(false, Integer.parseInt(msgIdx), levelForUpdate);
 				}
 			}
 			else if (problem != null && problem.equals(RLMarkerAttribute.ERR_NO_RL)) {
@@ -97,19 +97,136 @@ public class RLQuickFix implements IMarkerResolution {
 				String msgIdx = (String) marker.getAttribute(RLMarkerAttribute.RL_MSG_INDEX);
 
 				boolean isok = findMethod(marker.getResource(), Integer.parseInt(methodIdx));
-				
 
 				if (isok) {
 					this.addOrRemoveRLAnnotation(true, Integer.parseInt(msgIdx));
 				}
 
 			}
+			//RL順序對調
+			else if (problem != null && problem.equals(RLMarkerAttribute.ERR_RL_INSTANCE))
+			{
+				String methodIdx = (String) marker.getAttribute(RLMarkerAttribute.RL_METHOD_INDEX);
+				String msgIdx = (String) marker.getAttribute(RLMarkerAttribute.RL_MSG_INDEX);
+				findMethod(marker.getResource(), Integer.parseInt(methodIdx));
 
+				//把currentMethodRLList存成Array + 1(加1為排序時可調整的空間)
+				RLMessage[] newRLList = new RLMessage[currentMethodRLList.size()+1];
+				//currentMethodRLList放入Array中
+				for (int i=0;i < currentMethodRLList.size();i++)
+					newRLList[i] = currentMethodRLList.get(i);
+				//在新增調整空間放入資料，使它不為Null，才不會出錯
+				newRLList[currentMethodRLList.size()] = newRLList[0];
+
+				//把RL List重新排序，若位置正確把結果存到currentMethodRLList中
+				permutation(newRLList,1);
+
+				//更新全部的 RL Annotation List
+				updateRLAnnotation(true, Integer.parseInt(msgIdx), levelForUpdate);
+				
+			}
 		}
 		catch (CoreException e) {
 			ErrorLog.getInstance().logError("[RLQuickFix]", e);
 		}
 
+	}
+
+	/**
+	 * 把RL List重新排序，若位置正確把結果存到currentMethodRLList中
+	 * @param newRLList
+	 * 				新排序的RL Annotation List	
+	 * @param i
+	 * 				排列群組位置
+	 */
+	private void permutation(RLMessage[] newRLList, int i) {
+		//排列中
+		if(i < newRLList.length - 1) {
+    		for(int j = i; j <= newRLList.length - 1; j++) {
+    			RLMessage tmp = newRLList[j];
+                //旋轉該區段最右邊數字至最左邊
+                for(int k = j; k > i; k--)
+                	newRLList[k] = newRLList[k-1];
+                newRLList[i] = tmp;
+                permutation(newRLList, i+1);
+                //還原
+                for(int k = i; k < j; k++)
+                	newRLList[k] = newRLList[k+1];
+                newRLList[j] = tmp;
+        	}
+   		//排列完成
+    	} else {
+        	//若RL Annotation List順序全部正確，把它記錄到currentMethodRLList裡
+        	if (!isAllRLListCorrect(newRLList)) {
+        		currentMethodRLList.clear();
+        		for(int j = 1; j <= newRLList.length - 1; j++)
+        			currentMethodRLList.add(newRLList[j]);
+        	}
+        }
+    }
+	
+	/**
+	 * 判斷所有的RL List位置是否正確
+	 * 
+	 * @param newRLList
+	 * 		新位置的RL Annotation List
+	 * @return
+	 * 		RL List順序是否全部正確
+	 */
+	private boolean isAllRLListCorrect(RLMessage[] newRLList)
+	{
+		int msg1 = 0;
+		for (int i=1;i<newRLList.length;i++) {
+			msg1++;
+			int k = 1;
+			for (int j=1;j<newRLList.length;j++) {
+				if (msg1 >= k++)
+					continue;
+
+				//判斷父類別是否在子類別前
+				boolean isErr = isParentFrontSon(newRLList[j].getTypeBinding(),newRLList[i].getTypeBinding().getQualifiedName());
+				if (isErr)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 判斷父類別是否在子類別前
+	 * 
+	 * @param typeBinding
+	 * 			父類別
+	 * @param typeName
+	 * 			子類別名稱
+	 * @return
+	 * 			父類別是否在子類別前
+	 */
+    public boolean isParentFrontSon(ITypeBinding typeBinding,String typeName)
+	{
+		if (typeBinding == null || typeName == null)
+			return false;
+
+		String qname = typeBinding.getQualifiedName();
+		if (qname.equals(typeName))
+			return true;
+
+		// 判斷父類別
+		ITypeBinding superClass = typeBinding.getSuperclass();
+		if (superClass != null) {
+			if (superClass.getQualifiedName().equals(typeName))
+				return true;
+		}
+
+		// 判斷介面
+		ITypeBinding[] interfaceType = typeBinding.getInterfaces();
+		if (interfaceType != null && interfaceType.length > 0) {
+			for (int i = 0, size = interfaceType.length; i < size; i++) {
+				if (interfaceType[i].getQualifiedName().equals(typeName))
+					return true;
+			}
+		}
+		return false;
 	}
 
 	private boolean findMethod(IResource resource, int methodIdx) {
@@ -144,7 +261,6 @@ public class RLQuickFix implements IMarkerResolution {
 						currentMethodExList = checker.check(visitor);
 						return true;
 					}
-
 				}
 			}
 			catch (Exception ex) {
@@ -251,42 +367,23 @@ public class RLQuickFix implements IMarkerResolution {
 				method.modifiers().add(0, root);
 			}
 
-			Document document = applyChange();
-
-			//取得目前的EditPart
-			IEditorPart editorPart = EditorUtils.getActiveEditor();
-			ITextEditor editor = (ITextEditor) editorPart;
-
-			//取得Method的起點位置
-			int srcPos = currentMethodNode.getStartPosition();
-			//用Method起點位置取得Method位於第幾行數(起始行數從0開始，不是1，所以減1)
-			int numLine = this.actRoot.getLineNumber(srcPos)-1;
-
-			//如果有import Robustness或RL的宣告行數就加1
-			if(!isImportRobustnessClass)
-				numLine++;
-			if(!isImportRLClass)
-				numLine++;
-
-			//取得行數的資料
-			IRegion lineInfo = null;
-			try {
-				lineInfo = document.getLineInformation(numLine);
-			} catch (BadLocationException e) {
-				logger.error("[BadLocation] EXCEPTION ",e);
-			}
-			
-			//反白該行 在Quick fix完之後,可以將游標定位在Quick Fix那行
-			editor.selectAndReveal(lineInfo.getOffset(), lineInfo.getLength());
+			applyChange();
 		}
 		catch (Exception ex) {
 			logger.error("[addOrRemoveRLAnnotation] EXCEPTION ",ex);
 		}
 	}
 
-
+	/**
+	 * 更新RL Annotation
+	 * 
+	 * @param isAllUpdate
+	 * 		是否更新全部的Annotation
+	 * @param pos
+	 * @param level
+	 */
 	@SuppressWarnings("unchecked")
-	private void updateRLAnnotation(int pos, int level) {
+	private void updateRLAnnotation(boolean isAllUpdate, int pos, int level) {
 		try {
 
 			actRoot.recordModifications();
@@ -304,14 +401,17 @@ public class RLQuickFix implements IMarkerResolution {
 			ArrayInitializer rlary = ast.newArrayInitializer();
 			value.setValue(rlary);
 
+			//若全部更新，表示為currentMethodRLList排序，所以加入Annotation Library宣告
+			if (isAllUpdate)
+				addImportDeclaration();
+			
 			int msgIdx = 0;
 			for (RLMessage rlmsg : currentMethodRLList) {
-				if (msgIdx++ == pos) {
-
+				//若isAllUpdate表示為RL Annotation排序，排序不用更改level，所以使它不進入
+				if (msgIdx++ == pos && !isAllUpdate) {
 					rlary.expressions().add(getRLAnnotation(ast, level, rlmsg.getRLData().getExceptionType()));
 				}
 				else {
-
 					rlary.expressions().add(
 							getRLAnnotation(ast, rlmsg.getRLData().getLevel(), rlmsg.getRLData().getExceptionType()));
 				}
@@ -351,7 +451,6 @@ public class RLQuickFix implements IMarkerResolution {
 	 *            例外類別
 	 * @return NormalAnnotation AST Node
 	 */
-
 	@SuppressWarnings("unchecked")
 	private NormalAnnotation getRLAnnotation(AST ast, int levelVal, String exClass) {
 		NormalAnnotation rl = ast.newNormalAnnotation();
@@ -376,7 +475,7 @@ public class RLQuickFix implements IMarkerResolution {
 	/**
 	 * 將要變更的資料寫回至Document中
 	 */
-	private Document applyChange()
+	private void applyChange()
 	{
 		//寫回Edit中
 		try{
@@ -385,11 +484,43 @@ public class RLQuickFix implements IMarkerResolution {
 			TextEdit edits = actRoot.rewrite(document, cu.getJavaProject().getOptions(true));
 			edits.apply(document);
 			cu.getBuffer().setContents(document.get());
-			return document;
+			
+			setCursorLocation(document);
 		}
 		catch(Exception ex){
 			logger.error("[RLQuickFix] EXCEPTION ",ex);
 		}
-		return null;
+	}
+
+	/**
+	 * 游標定位(定位到RL Annotation那行)
+	 * @param document
+	 */
+	private void setCursorLocation(Document document) {
+		//取得目前的EditPart
+		IEditorPart editorPart = EditorUtils.getActiveEditor();
+		ITextEditor editor = (ITextEditor) editorPart;
+
+		//取得Method的起點位置
+		int srcPos = currentMethodNode.getStartPosition();
+		//用Method起點位置取得Method位於第幾行數(起始行數從0開始，不是1，所以減1)
+		int numLine = this.actRoot.getLineNumber(srcPos)-1;
+
+		//如果有import Robustness或RL的宣告行數就加1
+		if(!isImportRobustnessClass)
+			numLine++;
+		if(!isImportRLClass)
+			numLine++;
+
+		//取得行數的資料
+		IRegion lineInfo = null;
+		try {
+			lineInfo = document.getLineInformation(numLine);
+		} catch (BadLocationException e) {
+			logger.error("[BadLocation] EXCEPTION ",e);
+		}
+		
+		//反白該行 在Quick fix完之後,可以將游標定位在Quick Fix那行
+		editor.selectAndReveal(lineInfo.getOffset(), lineInfo.getLength());
 	}
 }
