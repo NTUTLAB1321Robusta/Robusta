f5f84bf57d44bc69a707acf493f5ca192b1f0d38
charles <charles@98712cce-040b-4e5c-8357-9a3231c88d43>
2012-08-30 07:55:41 +0000

    ASTNodeFinder加強版
    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT_branches/20120828Charles@2089 98712cce-040b-4e5c-8357-9a3231c88d43

--- test/ntut/csie/filemaker/ASTNodeFinder.java
+++ test/ntut/csie/filemaker/ASTNodeFinder.java
@@ -1,6 +1,8 @@
 package ntut.csie.filemaker;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ntut.csie.rleht.common.RLBaseVisitor;
 import ntut.csie.robusta.util.PathUtils;
@@ -15,6 +17,7 @@ import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 
 /**
  * 從指定的專案中的類別取得CompilationUnit
@@ -79,18 +82,36 @@ public class ASTNodeFinder {
 	 * @param methodName
 	 * @return
 	 */
-	public static MethodDeclaration getMethodDeclarationNodeByName(Class<?> clazz, String projectName, String methodName) {
+	public static MethodDeclaration getMethodDeclarationNodeByName(
+			Class<?> clazz, String projectName, String methodName) {
 		CompilationUnit compilationUnit = getCompilationUnit(clazz, projectName);
 		return getMethodDeclarationNodeByName(compilationUnit, methodName);
 	}
-	
-	public static MethodDeclaration getMethodDeclarationNodeByName(CompilationUnit compilationUnit, String methodName) {
-		NameOfMethodDeclarationVisitor nameOfMethodDeclarationVisitor = new ASTNodeFinder().new NameOfMethodDeclarationVisitor(
+
+	public static MethodDeclaration getMethodDeclarationNodeByName(
+			CompilationUnit compilationUnit, String methodName) {
+		NameOfMethodDeclarationVisitor nameOfMethodDeclarationVisitor = 
+			new ASTNodeFinder().new NameOfMethodDeclarationVisitor(
 				compilationUnit, methodName);
 		compilationUnit.accept(nameOfMethodDeclarationVisitor);
-		return nameOfMethodDeclarationVisitor.getFoundNode();		
+		return nameOfMethodDeclarationVisitor.getFoundNode();
 	}
 	
+	public static List<MethodInvocation> getMethodInvocationByMethodNameAndCode(
+			Class<?> clazz, String projectName, String methodName, String code) {
+		CompilationUnit compilationUnit = getCompilationUnit(clazz, projectName);
+		return getMethodInvocationByMethodNameAndCode(compilationUnit, methodName, code);
+	}
+	
+	public static List<MethodInvocation> getMethodInvocationByMethodNameAndCode(
+			CompilationUnit compilationUnit, String methodName, String code) {
+		CodeOfMethodInvocationVisitor codeOfMethodInvocationVisitor = 
+			new ASTNodeFinder().new CodeOfMethodInvocationVisitor(
+				compilationUnit, methodName, code);
+		compilationUnit.accept(codeOfMethodInvocationVisitor);
+		return codeOfMethodInvocationVisitor.getFoundNodes();
+	}
+
 	/**
 	 * 利用Method Name找到指定的MethodDeclaration Node。<br />
 	 * Known Issue: 如果Class中擁有兩個使用相同名稱的Method，只會回傳最先找到的那個。
@@ -98,18 +119,18 @@ public class ASTNodeFinder {
 	 *
 	 */
 	public class NameOfMethodDeclarationVisitor extends ASTVisitor {
-		String nodeName;
+		String methodName;
 		MethodDeclaration foundNode;
 		CompilationUnit astRoot;
 		
 		public NameOfMethodDeclarationVisitor(CompilationUnit compilationUnit, String nodeName) {
 			astRoot = compilationUnit;
-			this.nodeName = nodeName;
+			this.methodName = nodeName;
 			foundNode = null;
 		}
 		
 		public boolean visit(MethodDeclaration node) {
-			if(node.getName().toString().equals(nodeName)) {
+			if(node.getName().toString().equals(methodName)) {
 				foundNode = node;
 			}
 			return false;
@@ -117,6 +138,46 @@ public class ASTNodeFinder {
 		
 		public MethodDeclaration getFoundNode() {
 			return foundNode;
+		}
+	}
+	
+	/**
+	 * 利用MethodName與程式碼片段找到所屬的MethodInvocation node。
+	 * 因為符合的程式碼片段可能有多個，所以回傳找到的MethodInvocation Node以List表示。
+	 * 如果程式碼片段所符合的Node不是MethodInvocation，
+	 * @author charles
+	 *
+	 */
+	public class CodeOfMethodInvocationVisitor extends ASTVisitor {
+		String methodName;
+		String codeName;
+		List<MethodInvocation> foundNodeList;
+		CompilationUnit astRoot;
+		
+		public CodeOfMethodInvocationVisitor(CompilationUnit compilationUnit,
+				String methodName, String codeName) {
+			astRoot = compilationUnit;
+			this.methodName = methodName;
+			this.codeName = codeName;
+			foundNodeList = new ArrayList<MethodInvocation>();
+		}
+		
+		public boolean visit(MethodDeclaration node) {
+			if(node.getName().toString().equals(methodName)) {
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean visit(MethodInvocation node) {
+			if(node.toString().equals(codeName)) {
+				foundNodeList.add(node);
+			}
+			return false;
+		}
+		
+		public List<MethodInvocation> getFoundNodes() {
+			return foundNodeList;
 		}
 	}
 	
