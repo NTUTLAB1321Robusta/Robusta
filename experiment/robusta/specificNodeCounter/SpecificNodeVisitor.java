cff9ad7d268c908be7a6a4f5a5d0955c87035529
pig211 <pig211@98712cce-040b-4e5c-8357-9a3231c88d43>
2013-08-22 03:06:49 +0000

    補充SpecificNodeVisitor的註解
    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT@2795 98712cce-040b-4e5c-8357-9a3231c88d43

--- experiment/robusta/specificNodeCounter/SpecificNodeVisitor.java
+++ experiment/robusta/specificNodeCounter/SpecificNodeVisitor.java
@@ -4,10 +4,21 @@ import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 
 /**
+ * 根據建構時的設定偵測檔案中特定節點的個數
  * @author pig
  */
 public class SpecificNodeVisitor extends ASTVisitor {
 
+	/*
+	 * 目前SpecificNodeCounter無法利用此class，
+	 * 要使用時可以先利用 RLBuilder，
+	 * 加入以下程式碼
+		private int number = 0;
+		SpecificNodeVisitor snVisitor = new SpecificNodeVisitor(ASTNode.CATCH_CLAUSE);
+		root.accept(snVisitor);
+		number += snVisitor.getCount();  
+	 */
+	
 	private int specificNodeType;
 	private int counter;
 	
