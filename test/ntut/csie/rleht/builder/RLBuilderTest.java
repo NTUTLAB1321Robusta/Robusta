76e9c0c8a268eecfd2b5dc35338f41916b8d7f5d
Kimthoasp90 <Kimthoasp90@98712cce-040b-4e5c-8357-9a3231c88d43>
2013-10-03 13:06:03 +0000

    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT@2889 98712cce-040b-4e5c-8357-9a3231c88d43

--- test/ntut/csie/rleht/builder/RLBuilderTest.java
+++ test/ntut/csie/rleht/builder/RLBuilderTest.java
@@ -1,9 +1,12 @@
 package ntut.csie.rleht.builder;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.lang.reflect.Method;
 
 import ntut.csie.csdet.preference.RobustaSettings;
+import ntut.csie.csdet.visitor.UserDefinedMethodAnalyzer;
 import ntut.csie.filemaker.JavaFileToString;
 import ntut.csie.filemaker.JavaProjectMaker;
 
@@ -11,7 +14,9 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RLBuilderTest {
@@ -21,11 +26,17 @@ public class RLBuilderTest {
 	CompilationUnit compilationUnit;
 
 	String projectName;
+	RobustaSettings robustaSettings ;
 
 	public RLBuilderTest() {
-		projectName = "FirstTest";
+		
 	}
-
+	@Before
+	public void setUp() throws Exception {
+		projectName = "FirstTest";
+		
+	}
+		
 	@Test
 	public void testIsIResourceNeedToBeDetectedWithNonIFile() throws Exception {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
@@ -78,14 +89,15 @@ public class RLBuilderTest {
 				.getRoot()
 				.getProject(projectName)
 				.getFile("./srcTest/TestTwo.java");
-
-		String fileName  = ResourcesPlugin
-		.getWorkspace().getRoot().getLocation().toOSString()
-		+ File.separator + projectName + ".xml";
+		String path=UserDefinedMethodAnalyzer.getRobustaSettingXMLPath(projectName);
 		
-		RobustaSettings projectSettings = new RobustaSettings(fileName,projectName);
-		projectSettings.setProjectDetectAttribute("srcTest", "enable", false);
-		projectSettings.writeNewXMLFile(fileName);
+		File file=new File(getProjectPath(projectName));
+		file.mkdir();
+		
+		robustaSettings = new RobustaSettings(path, projectName);
+		robustaSettings.getProjectDetect("srcTest");
+		robustaSettings.setProjectDetectAttribute("srcTest", "enable", false);
+		robustaSettings.writeNewXMLFile(path);
 
 		Method isIResourceNeedToBeDetected = RLBuilder.class.getDeclaredMethod(
 				"isIResource" +
@@ -99,28 +111,48 @@ public class RLBuilderTest {
 	@Test
 	public void testIsIResourceNeedToBeDetectedWithUserWantToDetectedJavaFile()
 			throws Exception {
-			IWorkspace workspace = ResourcesPlugin.getWorkspace();
-			IResource resource = workspace
-					.getRoot()
-					.getProject(projectName)
-					.getFile("./srcTest/TestTwo.java");
-			String fileName  = ResourcesPlugin
-			.getWorkspace().getRoot().getLocation().toOSString()
-			+ File.separator + projectName + ".xml";
-			
-			RobustaSettings projectSettings = new RobustaSettings(fileName,"FirstTest");
-			
-			projectSettings.setProjectDetectAttribute("srcTest", "enable", true);
-			projectSettings.writeNewXMLFile(fileName);
-			Method isIResourceNeedToBeDetected = RLBuilder.class.getDeclaredMethod(
-					"isIResource" +
-					"NeedToBeDetected", IResource.class);
-			isIResourceNeedToBeDetected.setAccessible(true);
-			Assert.assertTrue((Boolean) isIResourceNeedToBeDetected.invoke(
-					rlBuilder, resource));
-			
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IResource resource = workspace
+				.getRoot()
+				.getProject(projectName)
+				.getFile("./srcTest/TestTwo.java");
+		String path=UserDefinedMethodAnalyzer.getRobustaSettingXMLPath(projectName);
+	
+		File file=new File(getProjectPath(projectName));
+		file.mkdir();
+		
+		robustaSettings = new RobustaSettings(path, projectName);
+		
+		robustaSettings.setProjectDetectAttribute("srcTest", "enable", false);
+		robustaSettings.writeNewXMLFile(path);
+
+		Method isIResourceNeedToBeDetected = RLBuilder.class.getDeclaredMethod(
+				"isIResource" +
+				"NeedToBeDetected", IResource.class);
+		isIResourceNeedToBeDetected.setAccessible(true);
+		Assert.assertFalse((Boolean) isIResourceNeedToBeDetected.invoke(
+				rlBuilder, resource));
+		
+		
+		robustaSettings.setProjectDetectAttribute("srcTest", "enable", true);
+		robustaSettings.writeNewXMLFile(path);
+
+		isIResourceNeedToBeDetected = RLBuilder.class.getDeclaredMethod(
+				"isIResource" +
+				"NeedToBeDetected", IResource.class);
+		isIResourceNeedToBeDetected.setAccessible(true);
+		Assert.assertTrue((Boolean) isIResourceNeedToBeDetected.invoke(
+				rlBuilder, resource));
 			
 
 	}
+	private String getProjectPath(String projectName) {
+		return ResourcesPlugin.getWorkspace().getRoot().getLocation()
+				.toOSString()
+				+ File.separator
+				+ projectName;
+				
+	}
+	
 
 }
