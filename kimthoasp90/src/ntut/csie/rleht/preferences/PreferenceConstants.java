8bef29a37ca8fbe9d03d3f9eaded60f31507719e
lemycanh <lemycanh@98712cce-040b-4e5c-8357-9a3231c88d43>
2013-10-23 03:01:12 +0000

    * Remove mis-behavior
    git-svn-id: http://140.124.181.95/svn/LabProjects/RLEHT@2915 98712cce-040b-4e5c-8357-9a3231c88d43

--- kimthoasp90/src/ntut/csie/rleht/preferences/PreferenceConstants.java
+++
@@ -1,9 +0,0 @@
-package ntut.csie.rleht.preferences;
-
-/**
- * Constant definitions for plug-in preferences
- */
-public class PreferenceConstants {
-	public static final String P_RLCHPF_STRING = "RLCallHierarchyPackageFiliter";
-	
-}
