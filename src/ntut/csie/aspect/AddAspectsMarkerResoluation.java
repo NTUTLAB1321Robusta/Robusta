1618d821c98f7ff6b0f619813cfc8df16989102b
steven <zx4567zx4567@gmail.com>
2016-05-28 16:24:49 +0800

    Complete to create a aspects file in specific path.

--- src/ntut/csie/aspect/AddAspectsMarkerResoluation.java
+++ src/ntut/csie/aspect/AddAspectsMarkerResoluation.java
@@ -1,5 +1,10 @@
 package ntut.csie.aspect;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +15,15 @@ import ntut.csie.robusta.codegen.QuickFixUtils;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CatchClause;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -58,105 +66,176 @@ public class AddAspectsMarkerResoluation implements IMarkerResolution,
 		int badSmellLineNumber = getBadSmellLineNumberFromMarker(marker);
 		List<TryStatement> tryStatements = getAllTryStatementOfMethodDeclaration(methodDeclarationWhichHasBadSmell);
 		// 比對壞味道行數及trystatement所在位置，鎖定該被inject的try statement
-		TryStatement tryStatementWillBeInject = getTargetTryStetment(tryStatements, badSmellLineNumber);
+		TryStatement tryStatementWillBeInject = getTargetTryStetment(
+				tryStatements, badSmellLineNumber);
 		// 取得會被try block接注的例外型別
-		List<CatchClause> catchClauses = tryStatementWillBeInject.catchClauses();
+		List<CatchClause> catchClauses = tryStatementWillBeInject
+				.catchClauses();
 		String exceptionType = getExceptionTypeOfCatchClauseWhichHasBadSmell(
 				badSmellLineNumber, catchClauses);
-		MethodInvocation methodWhichWillThrowSpecificException = getTheFirstMethodInvocationWhichWillThrowTheSameExceptionAsInput(exceptionType, tryStatementWillBeInject);
+		MethodInvocation methodWhichWillThrowSpecificException = getTheFirstMethodInvocationWhichWillThrowTheSameExceptionAsInput(
+				exceptionType, tryStatementWillBeInject);
 		// 搜尋try block拿出第一個會丟出例外的node
-		
-		
+
 		String injectedMethodReturnType = getMethodReturnType(methodWhichWillThrowSpecificException);
 		String objectTypeOfInjectedMethod = getTheObjectTypeOfMethodInvocation(methodWhichWillThrowSpecificException);
 		// 以這個node為主角，開始inject例外
-		
+
 		String badSmellType = "";
 		try {
-			badSmellType = (String)marker.getAttribute(RLMarkerAttribute.RL_MARKER_TYPE);
+			badSmellType = (String) marker
+					.getAttribute(RLMarkerAttribute.RL_MARKER_TYPE);
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		badSmellType = badSmellType.replace("_", "");
-		System.out.println(badSmellType);
-		String space = " ";
-		String and = "&&";
-		
+
 		String className = getClassNameOfMethodDeclaration(methodDeclarationWhichHasBadSmell);
-		String nameOfMethodWhichHasBadSmell = methodDeclarationWhichHasBadSmell.getName().toString();
+		String nameOfMethodWhichHasBadSmell = methodDeclarationWhichHasBadSmell
+				.getName().toString();
 		String returnTypeOfMethodWhichHasBadSmell = getMethodDeclarationReturnType(methodDeclarationWhichHasBadSmell);
-		String injectedMethodName = methodWhichWillThrowSpecificException.getName().toString();
-		//aspects file content parts
-		String aspectJClassTitle = "public aspect " + className +"Aspect {";
-		String pointCut = "pointcut find"+injectedMethodName+"("+objectTypeOfInjectedMethod+" object"+") : ";
-		String call = "call"+"("+injectedMethodReturnType+space+objectTypeOfInjectedMethod+"."+injectedMethodName+"(..))";
-		String target = "target" +"(object)";
-		String withInCode = "withincode"+"("+returnTypeOfMethodWhichHasBadSmell+space+
-				className+"."+nameOfMethodWhichHasBadSmell+"(..)"+");";
 		
-		String around = "void around("+objectTypeOfInjectedMethod+" object"+") throws "+exceptionType+" : find"+injectedMethodName+"(object) {";
-		String aroundContent = "boolean swich = false;"+
-							   "if(swich){"+
-							   "throw new "+exceptionType+"();"+
-							   "} else {"+
-							   "object."+injectedMethodName+"();"+
-							   "}"
-							 +"}"
-							+"}";
-		String aspectJFileConetent = aspectJClassTitle+pointCut+call+and+target+and+withInCode+around+aroundContent;
-		
-		
-		System.out.println("aspectJFileConetent" + aspectJFileConetent);
-		
-		for(String a :importObjects){
-			System.out.println(a);
-		}
-		
-		
+		String injectedMethodName = methodWhichWillThrowSpecificException
+				.getName().toString();
+		String packageChain = "ntut.csie.aspect."+ badSmellType;
 		project = marker.getResource().getProject();
 		javaproject = JavaCore.create(project);
-//		try {
-//			createDotJavaFile("aspectTest","aspectTest","aspectTest", marker);
-//		} catch (CoreException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
+		IPackageFragmentRoot root = getSourceFolderOfCurrentProject();
+		createPackage(packageChain, root);
+		String fileContent = buildUpAspectsFile(exceptionType, injectedMethodReturnType,
+				objectTypeOfInjectedMethod, className,
+				nameOfMethodWhichHasBadSmell,
+				returnTypeOfMethodWhichHasBadSmell, injectedMethodName, badSmellType, packageChain);
+		String projectPath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString();
+		String FilePath = root.getPath().makeAbsolute().toOSString();
+		System.out.println(projectPath+"\\"+FilePath+"\\ntut\\csie\\aspect"+"\\"+badSmellType+"\\"+className+"Aspect.aj");
+		WriteFile(fileContent, projectPath+"\\"+FilePath+"\\ntut\\csie\\aspect"+"\\"+badSmellType+"\\"+className+"Aspect.aj");
+//		 try {
+//		 createDotJavaFile("packageName","className","aspectTest", marker);
+//		 } catch (CoreException e) {
+//		 // TODO Auto-generated catch block
+//		 e.printStackTrace();
+//		 }
+
+	}
+
+	private String buildUpAspectsFile(String exceptionType,
+			String injectedMethodReturnType, String objectTypeOfInjectedMethod,
+			String className, String nameOfMethodWhichHasBadSmell,
+			String returnTypeOfMethodWhichHasBadSmell, String injectedMethodName, String badSmellType,
+			String packageChain) {
+		// aspects file content parts
+		String space = " ";
+		String and = "&&";
+		String aspectJClassTitle = "\r\n"+"public aspect " + className + "Aspect {";
+		String pointCut = "pointcut find" + injectedMethodName + "("
+				+ objectTypeOfInjectedMethod + " object" + ") : ";
+		String call = "call" + "(" + injectedMethodReturnType + space
+				+ objectTypeOfInjectedMethod + "." + injectedMethodName
+				+ "(..))";
+		String target = "target" + "(object)";
+		String withInCode = "withincode" + "("
+				+ returnTypeOfMethodWhichHasBadSmell + space + className + "."
+				+ nameOfMethodWhichHasBadSmell + "(..)" + ");";
+
+		String around = "void around(" + objectTypeOfInjectedMethod + " object"
+				+ ") throws " + exceptionType + " : find" + injectedMethodName
+				+ "(object) {";
+		String aroundContent = "\t" + "  boolean swich = false;" + "\r\n"
+				+ "\t" + "  if(swich){" + "\r\n" + "\t\t" + "throw new "
+				+ exceptionType + "();" + "\r\n" + "\t" + "  } else {" + "\r\n"
+				+ "\t\t" + "object." + injectedMethodName + "();" + "\r\n"
+				+ "\t" + "  }" + "\r\n" + "\t" + "}" + "\r\n" + "}";
+
+		String imports = "";
+		for (String importObj : importObjects) {
+			imports = imports + "import" + importObj + ";\r\n";
+		}
+		
+		String aspectJFileConetent = packageChain + "\r\n\r\n" + imports + aspectJClassTitle + "\r\n"
+				+ "\t" + pointCut + call + "\r\n" + "\t" + and + target
+				+ "\r\n" + "\t" + and + withInCode + "\r\n\r\n" + "\t" + around
+				+ "\r\n" + aroundContent;
+
+		System.out.println("aspectJFileConetent" + aspectJFileConetent);
+
+		return aspectJFileConetent;
+	}
+
+	public void WriteFile(String str, String path) {
+		
+		BufferedWriter writer = null;
+		try {
+			File file = new File(path);
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(file, false), "utf8"));
+			writer.write(str);
+			writer.newLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		} finally {
+			closeWriter(writer);
+		}
+	}
+
+	private void closeWriter(BufferedWriter writer) {
+		try {
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			ignore();
+		}
+	}
+
+	private void ignore() {
+	}
+	
+	private IPackageFragmentRoot getSourceFolderOfCurrentProject(){
+		IPackageFragmentRoot[] roots;
+		try {
+			roots = javaproject.getAllPackageFragmentRoots();
+			for(IPackageFragmentRoot root : roots){
+				if(root.getKind() == IPackageFragmentRoot.K_SOURCE){
+					return root;
+				}
+			}
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+//	private void createDotJavaFile(String packageName, String className,
+//			String content, IMarker marker) throws CoreException {
+//		String folderName = "aspects";
+//		IPackageFragmentRoot root = javaproject
+//				.getPackageFragmentRoot(folderName);
+//		IPackageFragment ipf;
+//		if (!root.exists()) {
+//			ipf = createPackage(packageName);
+//		} else {
+//			root.open(null);
+//			ipf = root.getPackageFragment(packageName);
 //		}
-		
-		
-		
-	}
-
-	private void createDotJavaFile(String packageName, String className,
-			String content, IMarker marker) throws CoreException {
-		String folderName = "aspects";
-		IPackageFragmentRoot root = javaproject
-				.getPackageFragmentRoot(folderName);
-		IPackageFragment ipf;
-		if (!root.exists()) {
-			ipf = createPackage(packageName);
-		} else {
-			root.open(null);
-			ipf = root.getPackageFragment(packageName);
-		}
-
-		if (!className.endsWith(ASPECTJ_FILE_EXTENSION)) {
-			className += ASPECTJ_FILE_EXTENSION;
-		}
-
-		ipf.createCompilationUnit(className, content, false, null);
-	}
+//
+//		if (!className.endsWith(ASPECTJ_FILE_EXTENSION)) {
+//			className += ASPECTJ_FILE_EXTENSION;
+//		}
+//
+//		ipf.createCompilationUnit(className, content, false, null);
+//	}
 
 	// packageName could be in format like a.b.c.d
-	private IPackageFragment createPackage(String packageName)
-			throws CoreException {
-		IFolder sourceFolder = project.getFolder("123");
-		if (!sourceFolder.exists()) {
-			createSourceFolder("123");
+	private void createPackage(String packageName, IPackageFragmentRoot root)  {
+		 try {
+			root.createPackageFragment(packageName, false, null);
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
 		}
-		IPackageFragmentRoot root = javaproject
-				.getPackageFragmentRoot(sourceFolder);
-		return root.createPackageFragment(packageName, false, null);
 	}
 
 	private void createSourceFolder(String folderName) throws CoreException {
@@ -181,9 +260,12 @@ public class AddAspectsMarkerResoluation implements IMarkerResolution,
 			MethodInvocation methodWhichWillThrowSpecificException) {
 		FindTheFirstExpressionObjectTypeOfMethodInvocationVisitor theFirstExpressionVisitor = new FindTheFirstExpressionObjectTypeOfMethodInvocationVisitor();
 		methodWhichWillThrowSpecificException.accept(theFirstExpressionVisitor);
-		String objectPackageName = theFirstExpressionVisitor.getTheFirstExpression().resolveTypeBinding().getPackage().toString().replace("package", "");
-		String objectName = theFirstExpressionVisitor.getTheFirstExpression().resolveTypeBinding().getName();
-		importObjects.add(objectPackageName+"."+objectName);
+		String objectPackageName = theFirstExpressionVisitor
+				.getTheFirstExpression().resolveTypeBinding().getPackage()
+				.toString().replace("package", "");
+		String objectName = theFirstExpressionVisitor.getTheFirstExpression()
+				.resolveTypeBinding().getName();
+		importObjects.add(objectPackageName + "." + objectName);
 		return objectName;
 	}
 
@@ -192,16 +274,19 @@ public class AddAspectsMarkerResoluation implements IMarkerResolution,
 	}
 
 	private String getMethodDeclarationReturnType(MethodDeclaration method) {
-		String objectPackageName = method.getName().resolveTypeBinding().getPackage().toString().replace("package", "");
+		String objectPackageName = method.getName().resolveTypeBinding()
+				.getPackage().toString().replace("package", "");
 		String objectName = method.getName().resolveTypeBinding().getName();
-		importObjects.add(objectPackageName+"."+objectName);
+		importObjects.add(objectPackageName + "." + objectName);
 		return objectName;
 	}
 
 	private String getClassNameOfMethodDeclaration(MethodDeclaration method) {
 		TypeDeclaration classOfMethod = (TypeDeclaration) method.getParent();
 		String className = classOfMethod.resolveBinding().getName().toString();
-		importObjects.add(classOfMethod.getName().resolveTypeBinding().getPackage().toString().replace("package", "")+"."+className);
+		importObjects.add(classOfMethod.getName().resolveTypeBinding()
+				.getPackage().toString().replace("package", "")
+				+ "." + className);
 		return className;
 	}
 
@@ -262,6 +347,9 @@ public class AddAspectsMarkerResoluation implements IMarkerResolution,
 			int catchClauseLineNumber = compilationUnit
 					.getLineNumber(catchBlock.getStartPosition());
 			if (badSmellLineNumber == catchClauseLineNumber) {
+				String objectPackageName = catchBlock.getException().getType().resolveBinding().getPackage().toString().replace("package", "");
+				String objectName = catchBlock.getException().getType().resolveBinding().getName();
+				importObjects.add(objectPackageName+"."+objectName);
 				return catchBlock.getException().getType().toString();
 			}
 		}
